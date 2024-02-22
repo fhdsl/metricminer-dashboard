8fa1f08831132d99a1e3ffcbae0a5ba4fce176ba
cansavvy <cansav09@gmail.com>
2024-02-22 15:21:30 -0500

    Fixing glitches

--- refresh-scripts/refresh-ga.R
+++ refresh-scripts/refresh-ga.R
@@ -14,7 +14,7 @@ dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 # Declare command line options
 option_list <- list(
   optparse::make_option(
-    c("--property_ids"),
+    c("--ga_property_ids"),
     type = "character",
     default = NULL,
     help = "Property ids for websites you want google analytics from"
@@ -25,7 +25,7 @@ option_list <- list(
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 
-ga_metrics <- get_all_ga_metrics(property_ids = opt$property_ids)
+ga_metrics <- get_all_ga_metrics(property_ids = opt$ga_property_ids)
 
 saveRDS(ga_metrics, file.path(output_dir, "ga_metrics.RDS"))
 
